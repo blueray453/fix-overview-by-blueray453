5bdc2304b40a8883cb10ee75ffea7caa0166090e
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-12-14 01:19:15 +0600

    Alhamdulillah, working

--- extension.js
+++ extension.js
@@ -6,16 +6,19 @@ import * as WindowPreview from 'resource:///org/gnome/shell/ui/windowPreview.js'
 import * as WorkspacesView from 'resource:///org/gnome/shell/ui/workspacesView.js';
 import * as WorkspaceThumbnail from 'resource:///org/gnome/shell/ui/workspaceThumbnail.js';
 import { setLogging, setLogFn, journal } from './utils.js';
+import { PrototypeInjector } from './PrototypeInjector.js';
 
-const _originalHideOverlay = WindowPreview.WindowPreview.prototype.hideOverlay;
-const _originalShowOverlay = WindowPreview.WindowPreview.prototype.showOverlay;
-const _originalWindowPreviewInit = WindowPreview.WindowPreview.prototype._init;
+// const _originalHideOverlay = WindowPreview.WindowPreview.prototype.hideOverlay;
+// const _originalShowOverlay = WindowPreview.WindowPreview.prototype.showOverlay;
+// const _originalWindowPreviewInit = WindowPreview.WindowPreview.prototype._init;
 
-const _originalUpdateShouldShow = WorkspaceThumbnail.ThumbnailsBox.prototype._updateShouldShow;
-const _originalUpdateWorkspacesState = WorkspacesView.WorkspacesView.prototype._updateWorkspacesState;
+// const _originalUpdateShouldShow = WorkspaceThumbnail.ThumbnailsBox.prototype._updateShouldShow;
+// const _originalUpdateWorkspacesState = WorkspacesView.WorkspacesView.prototype._updateWorkspacesState;
 
 export default class NotificationThemeExtension extends Extension {
   enable() {
+    this.injector = new PrototypeInjector();
+
     setLogFn((msg, error = false) => {
       let level;
       if (error) {
@@ -82,57 +85,100 @@ export default class NotificationThemeExtension extends Extension {
   }
 
   _WindowPreviewTitle() {
-    WindowPreview.WindowPreview.prototype.hideOverlay = function (...args) {
-      _originalHideOverlay.call(this, ...args);
+    this.injector.injectMultiple(WindowPreview.WindowPreview.prototype, {
+      hideOverlay: {
+        mode: 'after',
+        fn: function () {
+          this._title.set_opacity(255);
+        }
+      },
+      showOverlay: {
+        mode: 'after',
+        fn: function () {
+          this._title.set_opacity(255);
+        }
+      },
+      _init: {
+        mode: 'after',
+        fn: function () {
+          if (!this._title) return;
 
-      // Keep the title visible, don't animate it
-      this._title.set_opacity(255);
-    };
-
-    WindowPreview.WindowPreview.prototype.showOverlay = function (...args) {
-      _originalShowOverlay.call(this, ...args);
-
-      // Remove the title from Shell's animation handling
-      this._title.set_opacity(255);
-    };
-
-    WindowPreview.WindowPreview.prototype._init = function () {
-      // Call the original _init
-      _originalWindowPreviewInit.apply(this, arguments);
-
-      if (!this._title)
-        return;
-
-      this._title.show();
-
-      // Center the window title
-      const titleConstraints = this._title.get_constraints();
-      for (const constraint of titleConstraints) {
-        if (constraint instanceof Clutter.AlignConstraint &&
-          constraint.align_axis === Clutter.AlignAxis.Y_AXIS) {
-          constraint.set_factor(0.5); // 0=top, 0.5=center, 1=bottom
+          this._title.show();
+          this._title.get_constraints().forEach(constraint => {
+            if (constraint instanceof Clutter.AlignConstraint &&
+              constraint.align_axis === Clutter.AlignAxis.Y_AXIS) {
+              constraint.set_factor(0.5);
+            }
+          });
         }
       }
-    };
+    });
+    // WindowPreview.WindowPreview.prototype.hideOverlay = function (...args) {
+    //   _originalHideOverlay.call(this, ...args);
+
+    //   // Keep the title visible, don't animate it
+    //   this._title.set_opacity(255);
+    // };
+
+    // WindowPreview.WindowPreview.prototype.showOverlay = function (...args) {
+    //   _originalShowOverlay.call(this, ...args);
+
+    //   // Remove the title from Shell's animation handling
+    //   this._title.set_opacity(255);
+    // };
+
+    // WindowPreview.WindowPreview.prototype._init = function () {
+    //   // Call the original _init
+    //   _originalWindowPreviewInit.apply(this, arguments);
+
+    //   if (!this._title)
+    //     return;
+
+    //   this._title.show();
+
+    //   // Center the window title
+    //   const titleConstraints = this._title.get_constraints();
+    //   for (const constraint of titleConstraints) {
+    //     if (constraint instanceof Clutter.AlignConstraint &&
+    //       constraint.align_axis === Clutter.AlignAxis.Y_AXIS) {
+    //       constraint.set_factor(0.5); // 0=top, 0.5=center, 1=bottom
+    //     }
+    //   }
+    // };
   }
 
   _hideThumbnailsBox() {
-    WorkspaceThumbnail.ThumbnailsBox.prototype._updateShouldShow = function () {
-      this._shouldShow = false;
-      this.notify('should-show');
-    };
+    this.injector.override(
+      WorkspaceThumbnail.ThumbnailsBox.prototype,
+      '_updateShouldShow',
+      function () {
+        this._shouldShow = false;
+        this.notify('should-show');
+      }
+    );
+    // WorkspaceThumbnail.ThumbnailsBox.prototype._updateShouldShow = function () {
+    //   this._shouldShow = false;
+    //   this.notify('should-show');
+    // };
   }
 
   _scaleWorkspacesView() {
-    WorkspacesView.WorkspacesView.prototype._updateWorkspacesState = function (...args) {
-      // Call the original function first
-      _originalUpdateWorkspacesState.call(this, ...args);
+    this.injector.after(
+      WorkspacesView.WorkspacesView.prototype,
+      '_updateWorkspacesState',
+      function () {
+        this._workspaces.forEach(w => w.set_scale(0.96, 0.96));
+      }
+    );
+    // WorkspacesView.WorkspacesView.prototype._updateWorkspacesState = function (...args) {
+    //   // Call the original function first
+    //   _originalUpdateWorkspacesState.call(this, ...args);
 
-      // Apply custom scaling on top
-      this._workspaces.forEach(w => {
-        w.set_scale(0.96, 0.96);
-      });
-    };
+    //   // Apply custom scaling on top
+    //   this._workspaces.forEach(w => {
+    //     w.set_scale(0.96, 0.96);
+    //   });
+    // };
   }
 
   disable() {
@@ -141,11 +187,13 @@ export default class NotificationThemeExtension extends Extension {
       this._idleId = null;
     }
 
-    WindowPreview.WindowPreview.prototype.hideOverlay = _originalHideOverlay;
-    WindowPreview.WindowPreview.prototype.showOverlay = _originalShowOverlay;
-    WindowPreview.WindowPreview.prototype._init = _originalWindowPreviewInit;
+    this.injector.removeAllInjections();
 
-    WorkspaceThumbnail.ThumbnailsBox.prototype._updateShouldShow = _originalUpdateShouldShow;
-    WorkspacesView.WorkspacesView.prototype._updateWorkspacesState = _originalUpdateWorkspacesState;
+    // WindowPreview.WindowPreview.prototype.hideOverlay = _originalHideOverlay;
+    // WindowPreview.WindowPreview.prototype.showOverlay = _originalShowOverlay;
+    // WindowPreview.WindowPreview.prototype._init = _originalWindowPreviewInit;
+
+    // WorkspaceThumbnail.ThumbnailsBox.prototype._updateShouldShow = _originalUpdateShouldShow;
+    // WorkspacesView.WorkspacesView.prototype._updateWorkspacesState = _originalUpdateWorkspacesState;
   }
 }
