5e065b22700c904031303aad5db71ae1bb9a9047
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2026-02-03 19:55:45 +0600

    Alhamdulillah, stable

--- extension.js
+++ extension.js
@@ -77,7 +77,12 @@ export default class NotificationThemeExtension extends Extension {
   }
 
   _NoOverviewAtStartUp(){
-    this._idleId = GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
+    // PRIORITY_HIGH = -100
+    // PRIORITY_DEFAULT = 0
+    // PRIORITY_HIGH_IDLE = 100
+    // PRIORITY_DEFAULT_IDLE = 200
+    // PRIORITY_LOW = 300
+    this._idleId = GLib.idle_add(GLib.PRIORITY_HIGH, () => {
       Main.overview.hide();
       this._idleId = null;
       return GLib.SOURCE_REMOVE;
